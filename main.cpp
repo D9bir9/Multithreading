518b4caa3242868cc916ce0492c157bfc4e20079
Dabira <taiwoadejumo1234@gmail.com>
2026-09-09 09:18:56 +0000

    Condition Variables

--- main.cpp
+++ main.cpp
@@ -1,45 +1,68 @@
-// TOPIC: unique_lock in C++ (std::unique_lock<mutex> lock(m1))
+// TOPIC: Condition Variable In C++ Threading
 
-// Notes:
-// 1. The class unique_lock is a mutex ownersiop wrapper.
-// 2. It allows:
-//    a. having different locking strategies
-//    b. time-constrained attepts at locking (try_lock_for, try_lock_until)
-//    c. recursive lock
-//    d. transfer of lock ownership (move not copy)
-//    e. condition variables.
+// IMPORTANT POINT: CV are used for two purposes
+// A. Notify other threads
+// B. Waiting for some conditions
 
-// Locking Strategies
-//    Type               Effects
-// 1. defer_lock         do not acquire ownership of the mutex
-// 2. try_to_lock        try to acquire ownership of the mutex without blocking.
-// 3. adopt_lock         assumes the calling thread already has ownership of the mutex.
+// 1. Condition Variable allows running threads to wait on some conditions, and once those conditions are met, the waiting thread 
+//    is notified using:
+//      a. notify_one();
+//      b. notify_all();
+// 2. You need mutex to use condition variable.
+// 3. If some thread want to wait on some condition, then it has to do these things:
+//      a. Acquire the mutex lock using std::uniqui_lock<std::mutex> lock(m);.
+//      b. Execute wait, wait_for, or wait_until. The wait operation atomically release the mutex
+//         and suspend the execution of the thread.
+//      c. When the condition variable is notified, the thread is awakened, and the mutex is atomically reacquired.
+//         The thread should then check the condition and release waiting if the wakeup was spurious
 
+// NOTE:
+// 1. Condition variables are used to synchronize two or more threads.
+// 2. Best use case of condition variable is Producer/Consumer problem. 
 
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
-std::mutex m1;
-int buffer{};
+std::condition_variable cv;
+std::mutex m;
+long balance = 0;
 
-void task(const char* threadNumber, int loopFor){
-    std::unique_lock<std::mutex> lock(m1); // Automatically calls lock on mutex m1
-    // std::unique_lock<std::mutex> lock(m1, std::defer_lock); // Does not lock automatically
-    // lock.lock();
-    for (int i{}; i < loopFor; ++i){
-        buffer++;
-        std::cout << threadNumber << ": "<< buffer << std::endl;
+void addMoney(int amount){
+    std::lock_guard<std::mutex> lg(m);
+    if (amount > 0) {
+        balance += amount;
+        std::cout << amount 
+        << " deposited successfully!\nCurrent Balance : " 
+        << balance << std::endl;
+        cv.notify_one();
+    }
+    else{
+        std::cout << "Amount Must be greater than 0\n";
     }
 }
 
-int main(){
-    std::thread t1(task, "T0", 10);
-    std::thread t2(task, "T1", 10);
+void withdrawMoney(int amount){
+    std::unique_lock<std::mutex> ul(m);
+    cv.wait(ul, []{return (balance!=0)? true : false;});
+    if (balance >= amount){
+        balance -= amount;
+        std::cout << amount 
+        << " withdrawn successfully!\n";
+    }
+    else{
+        std::cout << "Insufficient balance\n";
+    }
+    std::cout << "Current Balance : " 
+        << balance << std::endl;
+}
 
+int main(){
+    std::thread t1(withdrawMoney, 500);
+    std::thread t2(addMoney, 500);
     t1.join();
     t2.join();
 
     return 0;
-
 }
